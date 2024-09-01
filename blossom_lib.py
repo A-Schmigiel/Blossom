4ea50e25afc8913cc779176b8289bd52df5129b2
A-Schmigiel <avianschmigiel@gmail.com>
2024-09-01 18:21:52 +0100

    completed practise project

---
+++ blossom_lib.py
@@ -0,0 +1,2 @@
+# code provided for the purpose of this practice project
+flower_definitions = [['begonia', 'cautiousness'], ['chrysanthemum', 'cheerfulness'], ['carnation', 'memories'], ['daisy', 'innocence'], ['hyacinth', 'playfulness'], ['lavender', 'devotion'], ['magnolia', 'dignity'], ['morning glory', 'unrequited love'], ['periwinkle', 'new friendship'], ['poppy', 'rest'], ['rose', 'love'], ['snapdragon', 'grace'], ['sunflower', 'longevity'], ['wisteria', 'good luck']]
